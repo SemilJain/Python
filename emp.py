d1db17dc1b3928e2ace5a62cd14a8a76d1ae06a2
Semil Jain <jainsemilv@gmail.com>
2019-07-04 09:35:15 +0530

    Revertback testcase

---
+++ emp.py
@@ -0,0 +1,14 @@
+import ast
+
+emp_set = set()
+temp_set = set()
+
+ast.literal_eval(input(""))
+print ("Git revert test sample") 
+'''n = int(input('Enter no. of employee pairs in the list - '))
+for x in range(n):
+	str1 = input('Enter employee pair: ')
+	emp_list.append(str1.split())
+
+#print (emp_list)
+'''
