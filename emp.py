c49524164e41a7f5ae27fb5d8fd76035ddf7015f
Semil Jain <jainsemilv@gmail.com>
2019-07-04 09:23:00 +0530

    Accidental commit test

---
+++ emp.py
@@ -0,0 +1,13 @@
+import ast
+
+emp_set = set()
+temp_set = set()
+
+ast.literal_eval(input("")) 
+'''n = int(input('Enter no. of employee pairs in the list - '))
+for x in range(n):
+	str1 = input('Enter employee pair: ')
+	emp_list.append(str1.split())
+
+#print (emp_list)
+'''
